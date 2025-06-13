c2c03f6e6988fb80cdb0ced0d287d10f96aaa748
yagojogador10 <yagosouzapereira4@gmail.com>
2025-06-13 17:34:00 -0300

    oi

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yagojogador10.github.io/' // Substitua 'yagojogador10.github.io' pelo nome do repositório no GitHub
+  base: '/yagojogador10.github.io/',
+  server: {
+    allowedHosts: [
+      'yagojogador10-github-io.onrender.com'
+    ]
+  }
 });
