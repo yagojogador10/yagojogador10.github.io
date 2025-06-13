fb50b06ac72571cd0cd03abb0bc2b8597021444e
yagojogador10 <yagosouzapereira4@gmail.com>
2025-06-13 16:27:34 -0300

    oi

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yagojogador.github.io/', // Substitua 'portfolio' pelo nome do repositório no GitHub
+  base: '/yagojogador10.github.io/', // Substitua 'portfolio' pelo nome do repositório no GitHub
 });
